8d9fbc24585c7176522fda15fa64046d792278a0
livalves <liviamabelle@gmail.com>
2025-03-20 15:52:28 -0300

    Respondendo exercicios 6 e 7

--- codigos/comb_dict.py
+++ codigos/comb_dict.py
@@ -6,3 +6,14 @@
 # d2 = {"a": 1, "b": 4, "d": 7}
 # Saída: {"a": 3, "b": 7, "c": 5, "d": 7}
 
+def combinando_dict(d1, d2):
+    dict_novo = {}
+    for k in set(d1) | set(d2):
+        dict_novo[k] = d1.get(k, 0) + d2.get(k, 0)
+    return dict(sorted(dict_novo.items()))
+
+d1 = {"a": 2, "b": 3, "c": 5}
+d2 = {"a": 1, "b": 4, "d": 7}
+dicionario = combinando_dict(d1, d2)
+print(dicionario)
+
--- codigos/top_frequentes.py
+++ codigos/top_frequentes.py
@@ -3,3 +3,16 @@
 # em ordem decrescente de frequência. Se houver empates, ordene pelo valor numérico.
 # Exemplo: [1, 3, 3, 2, 1, 1, 4, 4, 4, 2, 2, 2, 5, 5]
 # Saída: [2, 1, 4]
+
+
+from collections import Counter
+
+def top_frequentes(lista):
+    dict_contagem = Counter(lista)
+    dict_ord = sorted(dict_contagem.items(), key=lambda x: (-x[1], x[0]))
+    return [k for k, v in dict_ord[:3]]
+
+top = top_frequentes([1, 3, 3, 2, 1, 1, 4, 4, 4, 2, 2, 2, 5, 5])
+print(top)
+
+
